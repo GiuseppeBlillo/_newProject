216eeb7e613f2d94d48892b251634554616c3b48
Angelica Tempesta <angelica.tempesta96@gmail.com>
2023-07-25 18:13:36 +0200

    aggiunte varie classi ed enumerati

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import classes.Enumerations.CategoriaEnum;
+import classes.Utente;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<CategoriaEnum> listaPietro = new ArrayList<>();
+        listaPietro.add(10, CategoriaEnum.SPORTEFITNESS);
+        Utente u = new Utente(1,"pietro","croce", 32, "maschio", "pietrocross@cross.it", 800900313, listaPietro);
     }
 }
