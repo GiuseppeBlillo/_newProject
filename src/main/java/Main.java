45f7e75f40d20599a86f8d7c446f17733361beb1
Pietro_Croce <pcroce91@gmail.com>
2023-08-08 11:12:49 +0200

    Aggiunta dependency Hybernate core

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,5 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Utente u = new Utente(1, "pippo", "franco", 18, "pippofranco@intellij.com");
-        List<CategoriaEnum> listaInteressi = new ArrayList<>();
-        listaInteressi.add(CategoriaEnum.RELIGIONE);
-        listaInteressi.add(CategoriaEnum.SPORTEFITNESS);
-        listaInteressi.add(CategoriaEnum.RELIGIONE);
-
     }
 }
