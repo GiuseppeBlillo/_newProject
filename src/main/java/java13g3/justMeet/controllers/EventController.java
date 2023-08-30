46a32cd1b40338f01c243a5d27fe730a18c8b71c
Pietro_Croce <pcroce91@gmail.com>
2023-08-30 18:31:46 +0200

    Aggiunti controller per event

--- src/main/java/java13g3/justMeet/controllers/EventController.java
+++ src/main/java/java13g3/justMeet/controllers/EventController.java
@@ -1,10 +1,14 @@
 package java13g3.justMeet.controllers;
 
+import java13g3.justMeet.models.Event;
 import java13g3.justMeet.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+
+@RestController(value="/event")
 public class EventController {
     EventService eventService;
 
@@ -12,4 +16,37 @@ public class EventController {
     EventController(EventService eventService) {
         this.eventService = eventService;
     }
+
+    @GetMapping("/retrieve")
+    public ResponseEntity<?> getAllEvent(){
+        return ResponseEntity.ok(
+                eventService.retrieveAllEvents());
+    }
+
+    @GetMapping("/retrieve/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(eventService.retrieveEventById(id));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createEvent(@RequestParam Event event){
+        eventService.createNew(event);
+        return ResponseEntity.ok("Aggiunto evento");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteEventById(@PathVariable("id") Long id){
+        eventService.deleteEventById(id);
+        return ResponseEntity.ok("L'evento è stato cancellato");
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateEventById(
+            @PathVariable("id") Long id,
+            @RequestBody Event e){
+            eventService.updateEvent(id, e);
+        return ResponseEntity.ok("Rinnovato evento");
+    }
 }
+
+
