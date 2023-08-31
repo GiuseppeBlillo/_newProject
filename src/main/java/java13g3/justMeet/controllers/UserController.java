cdb11420923d1e4ffd89738cd71427b39d40ba35
Chax <chasasten@gmail.com>
2023-08-31 16:48:39 +0200

    aggiunti controller per user

--- src/main/java/java13g3/justMeet/controllers/UserController.java
+++ src/main/java/java13g3/justMeet/controllers/UserController.java
@@ -1,8 +1,12 @@
 package java13g3.justMeet.controllers;
 
+import java13g3.justMeet.models.User;
 import java13g3.justMeet.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 class UserController {
@@ -11,5 +15,33 @@ class UserController {
     @Autowired
     UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        userService.createUser(user);
+        return ResponseEntity.ok("Utente aggiunto");
+    }
+
+    @GetMapping("/retrieve")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.retrieveAllUsers());
+    }
+
+    @GetMapping("/retrieve/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(userService.retrieveUserById(id));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateUserById( @RequestBody User updatedUser) {
+        userService.updateUser(updatedUser);
+        return ResponseEntity.ok("Utente aggiornato");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable("id") Long id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.ok("L'utente è stato eliminato");
     }
 }
