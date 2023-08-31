1db85152e45b204df4655076e13cf727a1fbd32d
lillo <b.giuseppe@virgilio.it>
2023-08-31 17:53:55 +0200

    fixes

--- src/main/java/java13g3/justMeet/models/Registration.java
+++ src/main/java/java13g3/justMeet/models/Registration.java
@@ -2,30 +2,44 @@ package java13g3.justMeet.models;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "REGISTRATION")
 public class Registration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     @OneToMany
     @JoinColumn(name = "USER_ID")
-    private User userId;
+    private List<User> userList;
     @ManyToOne
     @JoinColumn(name = "EVENT_ID")
     private Event eventId;
 
-    public Registration(User userId, Event eventId) {
-        this.userId = userId;
+    public Registration(Long id, List<User> userList, Event eventId) {
+        this.id = id;
+        this.userList = userList;
         this.eventId = eventId;
     }
 
     public Registration() {
     }
 
-    public User getUserId() {
-        return userId;
+    public Long getId() {
+        return id;
     }
 
-    public void setUserId(User userId) {
-        this.userId = userId;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
     }
 
     public Event getEventId() {
