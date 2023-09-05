b54d3080ea5121376be33e8671fe93a5e249eb2f
Angelica Tempesta <angelica.tempesta96@gmail.com>
2023-09-05 19:08:44 +0200

    updage generale di controller,repo e service di User

--- src/main/java/java13g3/justMeet/repositories/UserRepository.java
+++ src/main/java/java13g3/justMeet/repositories/UserRepository.java
@@ -3,7 +3,15 @@ package java13g3.justMeet.repositories;
 import java13g3.justMeet.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByNickname(String nickname);
-    User findByEmail(String email);
+    Optional<List<User>> findByName(String name);
+    Optional<List<User>> findBySurname(String surname);
+    Optional<List<User>>  findByPhotoProfile(String photoProfile);
+    Optional<List<User>>  findByNickname(String nickname);
+    Optional<List<User>>  findByAge(int age);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
