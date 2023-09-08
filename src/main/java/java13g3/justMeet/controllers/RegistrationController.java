0ddfd0443d8f84239d8ed43117820fe5bf7bbab1
Angelica Tempesta <angelica.tempesta96@gmail.com>
2023-09-08 13:07:16 +0200

    rimozione codice morto

--- src/main/java/java13g3/justMeet/controllers/RegistrationController.java
+++ src/main/java/java13g3/justMeet/controllers/RegistrationController.java
@@ -18,30 +18,35 @@ class RegistrationController {
     RegistrationController(RegistrationService registrationService) {
         this.registrationService = registrationService;
     }
+
     @PostMapping("/create")
     public ResponseEntity<Registration> createRegistration(@RequestBody Registration registration) {
         return ResponseEntity.ok(registrationService.createRegistration(registration));
     }
+
     @GetMapping("/retrieve")
-    public ResponseEntity<List<Registration>> registrationList(){
+    public ResponseEntity<List<Registration>> registrationList() {
         return ResponseEntity.ok(registrationService.getAllRegistrations());
     }
+
     @GetMapping("/retrieve/{id}")
-    public ResponseEntity<Optional<Registration>> getRegistrationById(@PathVariable("id") Long registrationId){
+    public ResponseEntity<Optional<Registration>> getRegistrationById(@PathVariable("id") Long registrationId) {
         if (registrationService.getRegistrationById(registrationId).isPresent()) {
             return ResponseEntity.ok(registrationService.getRegistrationById(registrationId));
         } else {
             return ResponseEntity.notFound().build();
         }
     }
+
     @PutMapping("/update/{id}")
     public ResponseEntity<?> updateRegistration(@PathVariable("id") Long id, @RequestBody Registration registrationUp) {
-        if (registrationService.updateRegistration(id, registrationUp).isPresent()){
+        if (registrationService.updateRegistration(id, registrationUp).isPresent()) {
             return ResponseEntity.ok(registrationService.updateRegistration(id, registrationUp));
         } else {
             return ResponseEntity.notFound().build();
         }
     }
+
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteRegistrationById(@PathVariable("id") Long id) {
         if (registrationService.getRegistrationById(id).isPresent()) {
